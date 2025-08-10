1fcb2f246c5fd87f88efcffc54ff26a9da859870
Taslima-star <taslimanisan69@gmail.com>
2025-08-10 21:34:34 +0530

    Added icons to Footer using react-icons

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,14 +1,20 @@
 import React from 'react';
-
+import { FaGithub, FaLinkedin, FaEnvelope } from "react-icons/fa";
 
 function Footer() {
   return (
     <footer className="footer">
       <p>&copy; {new Date().getFullYear()} Taslima Nisan | Full Stack Web Developer</p>
       <p>
-        <a href="mailto:taslimanisan69@gmail.com">taslimanisan69@gmail.com</a> | 
-        <a href="https://github.com/Taslima-star" target="_blank" rel="noreferrer"> GitHub</a> | 
-        <a href="https://www.linkedin.com/in/taslima-nisan-217220259/" target="_blank" rel="noreferrer"> LinkedIn</a>
+        <a href="mailto:taslimanisan69@gmail.com">
+          <FaEnvelope /> Email
+        </a> | 
+        <a href="https://github.com/Taslima-star" target="_blank" rel="noreferrer">
+          <FaGithub /> GitHub
+        </a> | 
+        <a href="https://www.linkedin.com/in/taslima-nisan-217220259/" target="_blank" rel="noreferrer">
+          <FaLinkedin /> LinkedIn
+        </a>
       </p>
     </footer>
   );
